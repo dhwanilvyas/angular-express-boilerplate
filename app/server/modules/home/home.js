2a971e8e8984deb23bfe12796fbd30030a992270
dhwanilvyas <dhwanilvyas@outlook.com>
2016-09-17 22:30:55 +0530

    update server home module

--- app/server/modules/home/home.js
+++ app/server/modules/home/home.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var axios = require('axios');
 
 router.get('/', function (req, res) {
-  axios.get('http://api.dinetym.com/restaurants').then(function(response) {
-    res.render('home/home', { title:'Home', restaurants: response.data.data.data });
-  }).catch(function(err) {
-    console.log(err);
-  });
+  res.render('home/home', { title:'Home'});
 });
 
 module.exports = router;
